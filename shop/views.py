b2ba1aae6e8caaea26000958cf539d3fceeb542d
C-Anirudh <canirudh16@gmail.com>
2019-01-12 17:30:20 +0530

    basic shop view is added

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,13 @@
 from django.views.generic import TemplateView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from inventory.models import Product
 
 
-class ShopView(LoginRequiredMixin, TemplateView):
+class ShopView(LoginRequiredMixin, ListView):
     login_url = 'login'
+    model = Product
     template_name = 'shop.html'
+    context_object_name = 'products'
 
 
 class HomePageView(TemplateView):
